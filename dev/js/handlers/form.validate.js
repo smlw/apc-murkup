20a59c6d505be425b33c47d7c44a559b9efe34c8
Alexander Samoylov <smlwmy@gmail.com>
2018-12-27 23:45:28 +0500

    step2 handler done

--- dev/js/handlers/form.validate.js
+++ dev/js/handlers/form.validate.js
@@ -30,11 +30,42 @@ function regApp(elem) {
         if (validFlag) {
             registerApp.setDomainName(inputVal);
             $('.btn-contolls').find('.btn-next').addClass('d-block');
-            $('.result-message').append('<span class="text-success">OK</span>')
+            $('.result-message').append('<span class="text-success">OK</span>');
+            console.log(registerApp.getAppObject())
         } else {
             $('.btn-contolls').find('.btn-next').removeClass('d-block');
         }
-    }
+    };
+
+    this.checkRights = function(){
+        var data = {
+            url: registerApp.getAppObject().domainName,
+            secretKey: registerApp.getAppObject().secretKey
+        };
+
+        console.log(data)
+
+        $.ajax({
+            type: 'POST',
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            url: '/registerApp'
+        }).done(function (data) {
+            console.log(data);
+
+            if (!data.ok) {
+                console.log(false)
+                console.log(data.error);
+            } else {
+                console.log(true)
+                var rightsFlag = registerApp.getAppObject().checkRights;
+                if(rightsFlag){
+                    registerApp.setRights();
+                    $('.result-message').append('<span class="text-success">OK</span>');
+                }
+            }
+        });
+    };
 
     var self = this;
     elem.onclick = function (e) {
--- dev/js/handlers/register-app.js
+++ dev/js/handlers/register-app.js
@@ -1,5 +1,10 @@
 function RegisterApp() {
+    function randomString(length) {
+        return Math.round((Math.pow(36, length + 1) - Math.random() * Math.pow(36, length))).toString(36).slice(1);
+    }
+
     var appObject = {
+        secretKey: randomString(16),
         domainName: '',
         checkRights: false,
         DBData: {
